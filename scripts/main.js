0ef48f49992ecc97c1d4ebebb5467def55d41010
JamesCox22 <83503659+JamesCox22@users.noreply.github.com>
2021-07-19 21:28:18 -0230

    Changed 4 nav button functions to one single function

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,13 @@
 "use strict";
 
-const home = document.getElementById("home")
-const meetJess = document.getElementById("meetJess")
-const workWithMe = document.getElementById("workWithMe")
-const contact = document.getElementById("contact")
-
-function getContent(Home) {
-    page.innerHTML = "<p>My name is <strong>Jessica Cox</strong> and I am a Certified Life Coach.</p>"
-  
+function getContent(pageId) {
+    if (pageId === 'Home') {
+        page.innerHTML = "<p>My name is <strong>Jessica Cox</strong> and I am a <strong>Certified Life Coach!</strong>"
+}   else if (pageId === 'MeetJess') {
+        page.innerHTML = "<p>Hi! My name is Jessica Robyn Cox!</p>"
+}   else if (pageId === 'WorkWithMe') {
+        page.innerHTML = "<p>I am currently accepting applications for new clients...</p>"
+}   else if (pageId === 'Contact') {
+        page.innerHTML = "<p>Here is my number 780-555-2139</p>"
 }
-
-function visitMeetJess(){
-    page.innerHTML = "<p>Hi! My name is Jessica Robyn Cox!</p>"
-  
 }
-
-function visitWorkWithMe(){
-    page.innerHTML = "<p>I am currently accepting applications for new clients...</p>"
-    
-}
-
-function visitContact(){
-    page.innerHTML = "<p>Here is my number 780-555-2139</p>"
-  
-  }
