b05d1bcf567e91b9c8f5a613361eea0f1ccb9f4b
JamesCox22 <83503659+JamesCox22@users.noreply.github.com>
2021-07-20 13:44:21 -0230

    added semicolons

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,12 @@
 
 function getContent(pageId) {
     if (pageId === 'Home') {
-        page.innerHTML = "<p>My name is <strong>Jessica Cox</strong> and I am a <strong>Certified Life Coach!</strong>"
+        page.innerHTML = "<p>My name is <strong>Jessica Cox</strong> and I am a <strong>Certified Life Coach!</strong>";
 }   else if (pageId === 'MeetJess') {
-        page.innerHTML = "<p>Hi! My name is Jessica Robyn Cox!</p>"
+        page.innerHTML = "<p>Hi! My name is Jessica Robyn Cox!</p>";
 }   else if (pageId === 'WorkWithMe') {
-        page.innerHTML = "<p>I am currently accepting applications for new clients...</p>"
+        page.innerHTML = "<p>I am currently accepting applications for new clients...</p>";
 }   else if (pageId === 'Contact') {
-        page.innerHTML = "<p>Here is my number 780-555-2139</p>"
+        page.innerHTML = "<p>Here is my number 780-555-2139</p>";
 }
 }
