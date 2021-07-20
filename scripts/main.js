d6a3770e0582a8fde5f6becfc45f1bdbbc3f9830
JamesCox22 <83503659+JamesCox22@users.noreply.github.com>
2021-07-20 14:51:12 -0230

    changed function from 'else if' statements to 'switch' statement.

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,21 @@
 "use strict";
 
 function getContent(pageId) {
-    if (pageId === 'Home') {
-        page.innerHTML = "<p>My name is <strong>Jessica Cox</strong> and I am a <strong>Certified Life Coach!</strong>";
-}   else if (pageId === 'MeetJess') {
-        page.innerHTML = "<p>Hi! My name is Jessica Robyn Cox!</p>";
-}   else if (pageId === 'WorkWithMe') {
-        page.innerHTML = "<p>I am currently accepting applications for new clients...</p>";
-}   else if (pageId === 'Contact') {
-        page.innerHTML = "<p>Here is my number 780-555-2139</p>";
-}
+  switch (pageId) {
+    case 'Home':
+      page.innerHTML = "<p>My name is <strong>Jessica Cox</strong> and I am a <strong>Certified Life Coach!</strong>"
+      break;
+    case 'MeetJess':
+      page.innerHTML = "<p>Hi! My name is Jessica Robyn Cox!</p>"
+      break;
+    case 'WorkWithMe':
+      page.innerHTML = "<p>I am currently accepting applications for new clients...</p>"
+      break;
+    case 'Contact':
+      page.innerHTML = "<p>Here is my number 780-555-2139</p>"
+      break;
+    default:
+      page.innerHTML = "Page not found.";
+  }
+
 }
